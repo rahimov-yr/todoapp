e8289b03e6a1d97cd5138ab281cd7945111c1164
Yusuf Rahimov <yusuf.rahimov.yryr@gmail.com>
2023-04-29 15:27:46 -0600

    Version-01

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ const ToDoList = () => {
   const deleteHandle = (nameOfToDo) => {
     const newData = toDo.filter((item) => {
       // console.log(item)
+      // console.log(item)
       return item !== nameOfToDo
     })
 
